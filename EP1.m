758d8421e89b5053e093275fe93f79c3527b0b4c
Daniel Nery <danielnso@outlook.com>
2016-09-28 00:10:12 -0300

    Cabecalho

---
+++ EP1.m
@@ -0,0 +1,8 @@
+% PTC3213 - Eletromagnetismo
+% Exercício Programa 1
+% 14 de outubro de 2016
+%
+% Beatriz de Oliveira - 9
+% Daniel Nery Silva de Oliveira - 9349051
+% Mateus Almeida Barbosa - 9349072
+% Turma 3 - Professor Leb
