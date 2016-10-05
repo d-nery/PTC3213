288d35bd467111f8330f4d519cc64b764326619d
Daniel Nery <danielnso@outlook.com>
2016-10-05 16:24:33 -0300

    matriz de pontos

--- EP1.m
+++ EP1.m
@@ -30,5 +30,53 @@ h = (b-d)/2;
 f1 = figure;
 rectangle('Position', [0 0 a b])
 rectangle('Position', [g h c d])
-axis([-0.01 0.12 -0.01 0.07])
-axis equal
+axis([-0.01 0.12 -0.01 0.07]);
+axis equal;
+
+%%
+%delta
+delta = 1e-4;
+am = floor(a/delta) + 1;
+bm = floor(b/delta) + 1;
+gm = floor(g/delta) + 1;
+gcm = floor((g+c)/delta) + 1;
+bhdm = floor((b-d-h)/delta) + 1;
+bhm = floor((b-h)/delta) + 1;
+
+
+M = zeros(bm, am);
+for i = gm:gcm
+   M(bhdm, i) = 100;
+   M(bhm, i) = 100;
+end
+for i = bhdm:bhm
+   M(i, gm) = 100;
+   M(i, gcm) = 100;
+end
+
+i=0;
+while M(2,2) < 1e-10
+   for l = 2:bhdm-1
+       for c = 2:am-1
+           M(l,c) = (M(l-1,c) + M(l+1, c) + M(l, c - 1) + M(l, c + 1))/4;
+       end
+   end
+   for l = bhdm:bhm
+       for c = 2:gm-1
+           M(l,c) = (M(l-1,c) + M(l+1, c) + M(l, c - 1) + M(l, c + 1))/4;
+       end
+   end
+   for l = bhdm:bhm
+       for c = gcm+1:am-1
+           M(l,c) = (M(l-1,c) + M(l+1, c) + M(l, c - 1) + M(l, c + 1))/4;
+       end
+   end
+   for l = bhm+1:bm-1
+      for c = 2:am-1
+          M(l,c) = (M(l-1,c) + M(l+1, c) + M(l, c - 1) + M(l, c + 1))/4;
+      end
+   end
+   i = i+1
+end
+
+%%
