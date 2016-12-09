5dfd85feab08a100fbbaf0bc4b7a5775077fd3a6
Beatricce <beatriz.de.oli.s@gmail.com>
2016-12-08 23:53:18 -0200

    EP3 - A Esperança

---
+++ EP3.m
@@ -0,0 +1,143 @@
+% NUSP: 9350161 TURMA: LEB
+% C = 3, D = B - 3, G = 2, H = ( B - D )/2
+% Potencial vetor interno: 100 uWb/m² 
+% Potencial vetor externo: 0 
+% Equipotenciais de 10uWb/m
+
+a = 11;
+b = 6;
+c = 3;
+d = 4;
+g = 2;
+h = (b-d)/2;
+delta = 0.1;
+prec = 0.001;
+m = b/delta;          %nï¿½mero de linhas
+n = a/delta;          %nï¿½mero de colunas
+bve = g/delta;        %borda vertical da esquerda do condutor interno
+bvd = (g+c)/delta;    %borda vertical da direita do condutor interno
+bhs = (b-d-h)/delta;  %borda horizontal superior do condutor interno
+bhi = (b-h)/delta;    %borda horizontal inferior do condutor interno
+%sigma = 0.0032;        %valor da condutï¿½ncia
+%sigma1 = 0.003;       %valor da condutï¿½ncia no problema dual
+%epsilon = 1.9*8.854188E-12;
+%epsilon1 = 8.854188E-12;
+mi0 = 4*pi*1E-7; %permeabilidade magética no vácuo
+potInter = 100;
+
+A = zeros(m,n);
+A1 = zeros(m,n);
+
+
+%impï¿½e os potenciais nas bordas do condutor interno
+for i = bhs : bhi
+    for j = bve : bvd
+        A(i,j) = potInter;
+    end
+end
+
+%Realiza as iteraï¿½oes ate a maior diferenï¿½a entre duas iteraï¿½oes ser menor que prec
+erro = 1;
+while (erro > prec)
+    erro = 0;
+    for i = 2 : m - 1
+        for j = 2 : n - 1
+            if (A(i,j)~= 100)
+                valant = A(i,j);
+                A(i,j) = (A(i-1,j)+A(i+1,j)+A(i,j-1)+A(i,j+1))/4;
+                erro1 = abs(A(i,j)-valant);
+                if (erro1 > erro)
+                    erro = erro1;
+                end
+            end
+        end
+    end
+end
+
+A1 = round(A);  %escreve em outra matriz a matriz A com valores arredondados
+save('A1.txt','A1','-ascii');   %Salva A1 num arquivo texto
+
+%cï¿½lculo do campo magnético para uma superfície de interesse
+B=0.25*(2*A(1,1) + A(2,1) + 2*A(m,1) + 2*A(m,n) + 2*A(1,n) + A(2,n) - A(2,2) - 2*A(m-1,2) - 2*A(m-1,n-1) - A(2,n-1));
+
+for i=2:m-2
+    B = B + 0.5*(A(i,1) + A(i+1,1) + A(i,n) + A(i+1,n) - A(i,2) - A(i+1,2) - A(i,n-1) - A(i+1,n-1));
+end
+
+for j=2:n-2
+    B = B + 0.5*(A(m,j) + A(m,j+1) + A(1,j) + A(1,j+1) - A(m-1,j) - A(2,j) - A(m-1,j+1) - A(2,j+1));
+end
+B=-B;
+
+%cálculo da corrente (ou integral de linha em curva fechada do campo
+%magnético
+I = 1/mi0*B;
+
+L = potInter/I;    %cálculo da indutância
+
+% V2 = zeros(m,n);
+% 
+% %problema dual - para o traï¿½ado dos quadrados curvilï¿½neos
+% erro=1;
+% while (erro>prec)
+%     erro=0;
+%     for i=1:m
+%        for j=1:n
+%             valant=V2(i,j);
+%             if(i==m/2 && j<=bve)
+%                 V2(i,j) = 100;
+%             elseif (i==m/2 && j>=bvd)
+%                 V2(i,j) = 0;
+%             elseif (i<bhi && j>bve && j<bvd && i>bhs)
+%                 V2(i,j) = NaN;
+% 	        elseif (j==1 && i>1 && i<m)
+%                 V2(i,j) = (V2(i-1,j) + V2(i+1,j) + 2*V2(i,j+1))/4;
+%             elseif (j==1 && i==m)
+%                 V2(i,j) = (2*V2(i-1,j) + 2*V2(i,j+1))/4;
+%             elseif (i==m && j>1 && j<n)
+%                 V2(i,j) = (2*V2(i-1,j) + V2(i,j-1) + V2(i,j+1))/4;
+%             elseif (i==m && j==n)
+%                 V2(i,j) = (2*V2(i-1,j) + 2*V2(i,j-1))/4;
+%             elseif (i==1 && j==n)
+%                 V2(i,j) = (2*V2(i+1,j) + 2*V2(i,j-1))/4;
+%             elseif (j==n && i>1 && i<m)
+%                 V2(i,j) = (V2(i-1,j) + V2(i+1,j) + 2*V2(i,j-1))/4;
+%             elseif (i==1 && j>1 && j<n) 
+%                 V2(i,j) = (2*V2(i+1,j) + V2(i,j-1) + V2(i,j+1))/4;
+%             elseif (i==1 && j==1)
+%                 V2(i,j) = (2*V2(i+1,j) + 2*V2(i,j+1))/4;
+%             elseif (j==bvd && i>bhs && i<bhi && i~=m/2)
+%                 V2(i,j) = (V2(i-1,j) + V2(i+1,j) + 2*V2(i,j+1))/4;
+%             elseif (i==bhi && j>bve && j<bvd)
+%                 V2(i,j) = (2*V2(i+1,j) + V2(i,j-1) + V2(i,j+1))/4;
+%             elseif (j==bve && i>bhs && i<bhi && i~=m/2)
+%                 V2(i,j) = (V2(i-1,j) + V2(i+1,j) + 2*V2(i,j-1))/4;
+%             elseif (i==bhs && j>bve && j<bvd)
+%                 V2(i,j) = (2*V2(i-1,j) + V2(i,j+1) + V2(i,j-1))/4;
+%             else
+%                 V2(i,j) = (V2(i-1,j) + V2(i+1,j) + V2(i,j-1) + V2(i,j+1))/4;
+%             end
+%             if (i>=bhi || j<=bve || j>=bvd)
+%                 erro1=abs(V2(i,j)-valant);
+%                 if (erro1 > erro)
+%                     erro=erro1;
+%                 end
+%             end
+%        end
+%     end
+% end
+% 
+% %traï¿½a os quadrados curvilï¿½neos
+% m1 = 10;
+% n1 = round((m1/(R*sigma)+1)/2);
+% v1 = 0:10:100;
+% x= 100/n1;
+% v2 = 0:x:100;
+% v1(1) = 1e-6;
+% v2(1) = 1e-6;
+% contour (V,v1), axis equal;
+% hold on;
+% contour (V2,v2);
+% disp(R);
+% disp(C);
+% disp(R1);
